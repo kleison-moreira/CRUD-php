b225ba4388111417b7c14fc1fba3a6b1049dda3c
kleison <kleisonkeyboard@gmail.com>
2018-06-14 01:07:51 -0300

    primeiro commit

--- db/cadastro.sql
+++ db/cadastro.sql
@@ -4,13 +4,11 @@ default collate utf8_general_ci;
 use  cadastro;
 
 create table pessoa(
-id integer auto_increment unique primary key,
+id integer auto_increment primary key,
 nome varchar (100),
 email varchar (100)
 );
 
 
-insert into usuarios values ( 1,"admin", "1234");
- select * from pessoa;
- insert into pessoa values ( 2 , "kleison", "kleison97@hotmail.com");
- select * from pessoa;
+
+ 
